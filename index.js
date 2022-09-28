7388b6053485680e5bfe9bbf34c2f7d9454c8590
iPanda <aungkyawkhaing2000@gmail.com>
2022-09-29 01:00:19 +0630

    node

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 import http from "http"
 import fs from "fs"
 const server = http.createServer(function(req,res){
- 
-   fs.writeFile("writeAsync.txt","Aung Kyaw Khaing",(err)=>{
-      if(!err)console.log("success")
-   })
-   fs.writeFileSync("world.txt","akk");
-   fs.writeFileSync('tt.txt',"hel");
-   res.end()
+    fs.unlink('tt.txt',function(err){
+         if(!err)console.log("success")
+    })
+    res.end()
 })
 
 server.listen(8000,'127.0.0.1');
 console.log("server is running 127.0.0.1 port 8000")
-
 
