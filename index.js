1828e2425f388adc86f688c6861d778b3b9af351
iPanda007 <aungkyawkhaing2000@gmail.com>
2022-09-29 16:59:32 +0630

    pp

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import http from "http"
-import fs from "fs"
+import fs, { read } from "fs"
+import events from 'events'
+import e from "express";
 const server = http.createServer(function(req,res){
-    
-    res.end()
+ 
+    let eventEmitter = new events.EventEmitter();
+     eventEmitter.on("something",function(){
+        console.log("hello world")
+     })
+     eventEmitter.emit("something");
+     res.end();
 })
 
 server.listen(8000,'127.0.0.1');
